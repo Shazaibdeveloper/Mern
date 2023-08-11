091b2ab117972f91c57a7d2dec6782c8b6473bfd
Muhammad Shahzaib <shahzaibmuhammad627@gmail.com>
2023-08-11 17:47:36 +0500

    backend

--- server/router/Auth.js
+++ server/router/Auth.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const bodyParser = require('body-parser'); // Require the body-parser module
+router.use(bodyParser.json()); // Use the JSON parser
 
- 
+require("../db/Conn")
+ const User = require("../models/UserSchema")
 
 router.get('/', function (req, res) {
     res.send("Hello Worldsss !");
@@ -11,9 +14,22 @@ router.get('/register', function (req, res) {
     res.send("Hello register !");
 });
  
-router.post('/register', function (req, res) {
-    console.log(req.body);
-    res.json({message: req.body});
+router.post('/register', async function (req, res) {
+    try {
+        const { name, work, email, password, cpassword, phone } = req.body;
+
+        const userExists = await User.findOne({ email: email });
+        if (userExists) {
+            return res.status(422).json({ error: "email exists" });
+        }
+
+        const user = new User({ name, email, work, phone, password, cpassword });
+        await user.save();
+        return res.status(201).json({ message: "user registered successfully" });
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json({ error: "database error", message: err.message });
+    }
 });
 
 module.exports = router;
