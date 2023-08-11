fc268f34db54169989ec65dd042be2c9cb986e0f
Muhammad Shahzaib <shahzaibmuhammad627@gmail.com>
2023-08-11 11:14:11 +0500

    changes

--- server/App.js
+++ server/App.js
@@ -1,23 +1,11 @@
 var express = require('express');
 var app = express();
-const mongoose = require('mongoose');
+require('./db/Conn')
 const dotenv = require('dotenv');
 
-// Connection URL with placeholders for username, password, and database name
-const dbUsername = 'shahzaibmuhammad';
-const dbPassword = 'Blockchain2.0'; // Replace with your actual password
-const dbName = 'mernstack'; // Replace with your actual database name
 
-const db = `mongodb+srv://${dbUsername}:${dbPassword}@cluster0.nwcwrtp.mongodb.net/${dbName}?retryWrites=true&w=majority`;
-
-mongoose.connect(db, {
-    useNewUrlParser: true,
-     useUnifiedTopology: true,
- }).then(() => {
-    console.log("Connected to the database.");
-}).catch((err) => {
-    console.log("Connection error:", err);
-});
+dotenv.config({path:'./config.env'})
+ const PORT = process.env.PORT;
 
 const middleware = (req, res, next) => {
     console.log("Middleware executed.");
@@ -48,6 +36,6 @@ app.get('/logout', function (req, res) {
     res.send("Logout World!");
 });
 
-app.listen(3000, () => {
-    console.log("Server is listening on port 3000");
+app.listen(PORT, () => {
+    console.log(`Server is listening on ${PORT}`);
 });
