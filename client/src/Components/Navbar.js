f81e38778f1905896229a8907a26f2159c3ed96a
Muhammad Shahzaib <shahzaibmuhammad627@gmail.com>
2023-08-16 14:46:02 +0500

    ui

--- client/src/Components/Navbar.js
+++ client/src/Components/Navbar.js
@@ -1,9 +1,34 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
      <>
-     
+     <nav class="navbar navbar-expand-lg navbar-light bg-light px-5">
+  <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <Link class="nav-link" to="/home">Home</Link>
+      </li>
+      <li class="nav-item">
+        <Link class="nav-link" to="/about">About</Link>
+      </li>
+      <li class="nav-item">
+        <Link class="nav-link" to="/contact">Contact</Link>
+      </li>
+      <li class="nav-item">
+        <Link class="nav-link" to="/login">Login</Link>
+      </li>
+      <li class="nav-item">
+        <Link class="nav-link" to="/signin">Registers</Link>
+      </li>
+    </ul>
+  </div>
+</nav>
      </>
   )
 }
