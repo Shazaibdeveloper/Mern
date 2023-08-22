92b82a76332782393f2e9b820c94bf8d1348e362
Muhammad Shahzaib <shahzaibmuhammad627@gmail.com>
2023-08-22 09:34:50 +0500

    got data on home page

--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -1,17 +1,57 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
+  const [data, setData] = useState("");
+  const getData = async () => {
+    try {
+      const res = await fetch("/getdata", {
+        method: "GET",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        credentials: "include",
+      });
+
+      if (!res.ok) {
+        throw new Error(`Request failed with status: ${res.status}`);
+      }
+
+      const response = await res.json();
+      setData(response);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <>
-      <div className="Home-Main-container">
-        <div className="Main-container">
-          <h1>Welcome</h1>
-          <p>Your oasis of comfort and style</p>
-          <a className="button" href="#">
-            Get Started
-          </a>
+      {data ? (
+        <div className="Home-Main-container">
+          <div className="Main-container">
+            <h1>Welcome</h1>
+            <p>{data.name}</p>
+            <a className="button" href="#">
+              Get Started
+            </a>
+          </div>
         </div>
-      </div>
+      ) : (
+        <div className="Home-Main-container">
+          <div className="Main-container">
+            <h1>Welcome</h1>
+            <p>Sign in to get data</p>
+            <Link className="button" to="/signin">
+              Get Started
+            </Link>
+          </div>
+        </div>
+      )}
     </>
   );
 };
