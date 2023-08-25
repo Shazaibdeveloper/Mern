4c9dbdfe05f3c8c3594ac17bd7aba6bb60567deb
Muhammad Shahzaib <shahzaibmuhammad627@gmail.com>
2023-08-25 13:35:46 +0500

    contex working

--- client/src/App.js
+++ client/src/App.js
@@ -8,22 +8,30 @@ import Navbar from "./Components/Navbar";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Error from "./Components/Error";
 import Logout from "./Logout";
+import { createContext, useReducer } from "react";
+import { initial, reducer } from "./Reducer/UseReducers";
+
+export const userContext = createContext();
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initial);
+
   return (
     <>
       <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route index element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/signin" element={<Signin />} />
-          <Route path="/register" element={<Register />} />{" "}
-          <Route path="/logout" element={<Logout />} />
-          <Route path="*" element={<Error />} />
-        </Routes>
+        <userContext.Provider value={{ state, dispatch }}>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route index element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/signin" element={<Signin />} />
+            <Route path="/register" element={<Register />} />{" "}
+            <Route path="/logout" element={<Logout />} />
+            <Route path="*" element={<Error />} />
+          </Routes>
+        </userContext.Provider>
       </BrowserRouter>
     </>
   );
